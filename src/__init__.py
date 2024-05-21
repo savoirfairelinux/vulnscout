7670172a9f72151003725fdbb1e290297b6faf83
Louis Maillard <lmaillard@savoirfairelinux.com>
2024-05-21 14:55:50 -0400

    vuln: add class to handle vulnerability
    This class handle information about vulnerability and create
    a relationship with affected packages.
    Include unit-testing and documentation.
    Also added a bit of readme information in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+Code in this project follow the Model-View-Controller (MVC) pattern.
+On the left panel, you can access the models, views, and controllers part of the code.
+
+Classes in `models` folder are used to represent the data of the application. For example, Vulnerability, Package or CVSS classes are here.
+
+Classes in `controller` folder are used to manage list of data reprensented by models. For example, VulnerabilityController or PackageController classes are here. You will interact with them when adding, getting or filtering data.
+
+This app have two kind of views: web view, which are mostly JavaScript on browser side and few Python API code, and files.
+Most of the time, this python script will read and write file as I/O operations. So, you will find the file operations (parsing, formating) in the `views` folder.
+"""
