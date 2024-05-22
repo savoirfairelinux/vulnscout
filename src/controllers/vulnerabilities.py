d9b203e58e64e0868e084f99264742cca74a68be
Louis Maillard <lmaillard@savoirfairelinux.com>
2024-05-22 09:22:46 -0400

    vulnctrl: add vulnerability controller and tests
    Vulnerability controller is an essential part of the software. It
    provides the necessary API to interact with the vulnerabilities, storing
    them, updating them, and deleting them.
    It handle de-duplication and aliases for vulnerabilities.

---
+++ src/controllers/vulnerabilities.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from ..models.vulnerability import Vulnerability
+
+
+class VulnerabilitiesController:
+    """
+    A class to handle a list of vulnerabilities, de-duplicating them and handling low-level stuff.
+    Vulnerabilities can be added, removed, retrieved and exported or imported as dictionaries.
+    """
+
+    def __init__(self, pkgCtrl):
+        """Take an instance of PackagesController to resolve package dependencies as parameter."""
+        self.packagesCtrl = pkgCtrl
+        self.vulnerabilities = {}
+        """A dictionary of vulnerabilities, indexed by their id."""
+        self.alias_registered = {}
+
+    def get(self, vuln_id: str):
+        """Return a vulnerability by id (str) or None if not found. Also look for aliases."""
+        if vuln_id in self.vulnerabilities:
+            return self.vulnerabilities[vuln_id]
+        if vuln_id in self.alias_registered:
+            return self.vulnerabilities[self.alias_registered[vuln_id]]
+
+    def add(self, vulnerability: Vulnerability):
+        """Add a vulnerability to the list, merging it with an existing one if present."""
+        if vulnerability is None:
+            return
+        if vulnerability.id in self.vulnerabilities:
+            self.vulnerabilities[vulnerability.id].merge(vulnerability)
+            return
+        if vulnerability.id in self.alias_registered:
+            self.vulnerabilities[self.alias_registered[vulnerability.id]].merge(vulnerability)
+            return
+
+        for alias in vulnerability.aliases:
+            if alias in self.vulnerabilities:
+                self.register_alias(vulnerability.aliases, alias)
+                self.register_alias([vulnerability.id], alias)
+                self.vulnerabilities[alias].merge(vulnerability)
+                return
+            if alias in self.alias_registered:
+                self.register_alias(vulnerability.aliases, self.alias_registered[alias])
+                self.register_alias([vulnerability.id], self.alias_registered[alias])
+                self.vulnerabilities[self.alias_registered[alias]].merge(vulnerability)
+                return
+
+        self.register_alias(vulnerability.aliases, vulnerability.id)
+        self.vulnerabilities[vulnerability.id] = vulnerability
+
+    def register_alias(self, alias: list, vuln_id: str):
+        """Allow to register an list of alias pointing to a vulnerability id."""
+        for a in alias:
+            if a not in self.alias_registered and a != vuln_id:
+                self.alias_registered[a] = vuln_id
+
+    def remove(self, vuln_id: str) -> bool:
+        """Remove a vulnerability by id (str) and return True if removed, False if not found."""
+        if vuln_id in self.vulnerabilities:
+            del self.vulnerabilities[vuln_id]
+            aliases_to_remove = []
+            for alias, id in self.alias_registered.items():
+                if id == vuln_id:
+                    aliases_to_remove.append(alias)
+
+            for alias in aliases_to_remove:
+                del self.alias_registered[alias]
+            return True
+        return False
+
+    def to_dict(self) -> dict:
+        """Export the list of vulnerabilities as a dictionary of dictionaries."""
+        return {k: v.to_dict() for k, v in self.vulnerabilities.items()}
+
+    def from_dict(pkgCtrl, data: dict):
+        """
+        Import a list of vulnerabilities from a dictionary of dictionaries.
+        Require a PackagesController instance.
+        Return a new instance of VulnerabilitiesController.
+        """
+        item = VulnerabilitiesController(pkgCtrl)
+        for k, v in data.items():
+            item.add(Vulnerability.from_dict(v))
+        return item
+
+    def resolve_id(self, vuln_id: str) -> dict:
+        """Return a dictionary with the id of the vulnerability and a boolean to indicate if it is an alias."""
+        if vuln_id in self.vulnerabilities:
+            return {"is_alias": False, "id": vuln_id}
+        if vuln_id in self.alias_registered:
+            return {"is_alias": True, "id": self.alias_registered[vuln_id]}
+        return {"is_alias": False, "id": None}
+
+    def __contains__(self, item):
+        """
+        Check if the item is in the vulnerabilities list.
+        The item can be a Vulnerability class or a string representation of Vulnerability.id.
+        """
+        if isinstance(item, str):
+            if item in self.vulnerabilities:
+                return True
+            if item in self.alias_registered:
+                return True
+        elif isinstance(item, Vulnerability):
+            if item.id in self.vulnerabilities:
+                return True
+            if item.id in self.alias_registered:
+                return True
+        return False
+
+    def __len__(self):
+        """Return the number of vulnerabilities in the list."""
+        return len(self.vulnerabilities)
+
+    def __iter__(self):
+        """Allow iteration over the list of vulnerabilities."""
+        return iter(self.vulnerabilities.values())
---
+++ tests/integration_tests/test_vulnerabilities.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+import pytest
+from src.controllers.packages import PackagesController
+from src.controllers.vulnerabilities import VulnerabilitiesController
+from src.models.package import Package
+from src.models.vulnerability import Vulnerability
+
+
+@pytest.fixture
+def empty_controller():
+    return PackagesController()
+
+
+@pytest.fixture
+def pkg_ABC():
+    pkg = Package("abc", "1.0.0")
+    pkg.generate_generic_cpe()
+    pkg.generate_generic_purl()
+    return pkg
+
+
+@pytest.fixture
+def pkg_XYZ():
+    pkg = Package("xyz", "2.3.4")
+    pkg.generate_generic_cpe()
+    pkg.generate_generic_purl()
+    return pkg
+
+
+@pytest.fixture
+def pkg_controller(pkg_ABC, pkg_XYZ):
+    controller = PackagesController()
+    controller.add(pkg_ABC)
+    controller.add(pkg_XYZ)
+    return controller
+
+
+@pytest.fixture
+def vuln_controller_empty(pkg_controller):
+    return VulnerabilitiesController(pkg_controller)
+
+
+@pytest.fixture
+def vuln_123():
+    vuln = Vulnerability("CVE-123", "test", "test", "test")
+    vuln.add_url("https://cve.com/123")
+    vuln.add_text("CVE-123", "text1")
+    vuln.add_package("test@1.0.0")
+    vuln.add_advisory("advisory 1")
+    return vuln
+
+
+@pytest.fixture
+def vuln_456(vuln_123, pkg_ABC):
+    vuln = Vulnerability("CVE-456", "test", "test", "test")
+    vuln.add_alias(vuln_123.id)
+    vuln.add_related_vulnerability("CVE-000")
+    vuln.add_url("https://cve.com/456")
+    vuln.add_text("CVE-456", "text2")
+    vuln.add_package(pkg_ABC)
+    vuln.add_advisory("advisory 2")
+    return vuln
+
+
+@pytest.fixture
+def vuln_789(vuln_456, pkg_XYZ):
+    vuln = Vulnerability("CVE-789", "test", "test", "test")
+    vuln.add_alias(vuln_456.id)
+    vuln.add_url("https://cve.com/789")
+    vuln.add_package(pkg_XYZ)
+    return vuln
+
+
+@pytest.fixture
+def vuln_controller(pkg_controller, vuln_123):
+    controller = VulnerabilitiesController(pkg_controller)
+    controller.add(vuln_123)
+    return controller
+
+
+def test_vulnerability_not_present(vuln_controller, vuln_456):
+    """
+    GIVEN a VulnerabilitiesController instance
+    WHEN no parameters are passed
+    THEN check that the instance is created with empty attributes
+    """
+    assert vuln_controller.get(vuln_456.id) is None
+    assert vuln_456.id not in vuln_controller
+    assert vuln_456 not in vuln_controller
+    assert vuln_controller.resolve_id(vuln_456.id)["is_alias"] is False
+    assert vuln_controller.resolve_id(vuln_456.id)["id"] is None
+
+
+def test_add_vulnerability(vuln_controller, vuln_123):
+    """
+    GIVEN a VulnerabilitiesController instance
+    WHEN a vulnerability is added
+    THEN check that the vulnerability is added correctly to the controller
+    """
+    assert len(vuln_controller) == 1
+    assert vuln_controller.get(vuln_123.id) == vuln_123
+    assert vuln_123.id in vuln_controller
+    assert vuln_123 in vuln_controller
+    found_vuln = 0
+    for v in vuln_controller:
+        if v == vuln_123:
+            found_vuln = 1
+    assert found_vuln == 1
+
+
+def test_add_vulnerability_with_alias(vuln_controller, vuln_123, vuln_456):
+    """
+    GIVEN a VulnerabilitiesController instance
+    WHEN a vulnerability is added with an alias
+    THEN check that the vulnerability is added correctly to the controller
+    """
+    vuln_controller.add(vuln_456)
+    assert len(vuln_controller) == 1
+    assert vuln_controller.get(vuln_456.id) == vuln_123
+    assert vuln_456.id in vuln_controller
+    assert vuln_456 in vuln_controller
+    assert vuln_controller.resolve_id(vuln_123.id)["is_alias"] is False
+    assert vuln_controller.resolve_id(vuln_123.id)["id"] == vuln_123.id
+    assert vuln_controller.resolve_id(vuln_456.id)["is_alias"] is True
+    assert vuln_controller.resolve_id(vuln_456.id)["id"] == vuln_123.id
+
+
+def test_removing_vulnerability(vuln_controller, vuln_123, vuln_456):
+    """
+    GIVEN a VulnerabilitiesController instance
+    WHEN a vulnerability is added and removed
+    THEN check that the vulnerability and their alias are removed correctly from the controller
+    """
+    vuln_controller.add(vuln_456)
+    assert vuln_controller.remove(vuln_123.id) is True
+    assert len(vuln_controller) == 0
+    assert vuln_123.id not in vuln_controller
+    assert vuln_456.id not in vuln_controller
+    assert vuln_controller.remove(vuln_123.id) is False
+
+
+def test_export_import_vulnerabilities(vuln_controller, pkg_controller, vuln_123):
+    """
+    GIVEN a VulnerabilitiesController instance with vulnerabilities
+    WHEN the controller is exported and imported
+    THEN check that the controller is correctly exported and imported
+    """
+    new_vulnCtrl = VulnerabilitiesController.from_dict(pkg_controller, vuln_controller.to_dict())
+    assert len(new_vulnCtrl) == len(vuln_controller)
+    assert vuln_123 in new_vulnCtrl
+
+
+def test_add_vulnerability_already_present(vuln_controller, vuln_123, vuln_456, vuln_789):
+    """
+    GIVEN a VulnerabilitiesController instance with a vulnerability
+    WHEN the vulnerability is added again
+    THEN check that the vulnerability is merged with the existing one
+    """
+    vuln_controller.add(None)
+    assert len(vuln_controller) == 1
+    assert len(vuln_controller.alias_registered) == 0
+
+    vuln_controller.add(vuln_123)
+    assert len(vuln_controller) == 1
+    assert len(vuln_controller.alias_registered) == 0
+
+    vuln_controller.add(vuln_456)
+    assert len(vuln_controller) == 1
+    assert len(vuln_controller.alias_registered) == 1
+
+    vuln_controller.add(vuln_789)
+    assert len(vuln_controller) == 1
+    assert len(vuln_controller.alias_registered) == 2
+
+    vuln_controller.add(vuln_789)
+    assert len(vuln_controller) == 1
+    assert len(vuln_controller.alias_registered) == 2
