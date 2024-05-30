0bc01ac1a6f7ee459eb4a44fa8fe16e92df40d2f
Louis Maillard <lmaillard@savoirfairelinux.com>
2024-05-30 15:55:42 -0400

    grype: add support reading from grype outputs
    Added view to read and parse grype format.
    Able to read packages, vulnerabilities but no data about assessment.

---
+++ src/views/grype_vulns.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+from ..models.package import Package
+from ..models.vulnerability import Vulnerability
+from ..models.assessment import VulnAssessment
+from ..models.cvss import CVSS
+
+
+class GrypeVulns:
+    """
+    GrypeVulns class to handle grype vulnerabilities and parse it.
+    Support only reading and parsing from JSON format.
+    """
+
+    def __init__(self, controllers):
+        self.packagesCtrl = controllers["packages"]
+        self.vulnerabilitiesCtrl = controllers["vulnerabilities"]
+        self.assessmentsCtrl = controllers["assessments"]
+
+    def parse_artifact_section(self, artifact: dict) -> str:
+        """Parse the `artifact` part of grype JSON output."""
+        if "name" in artifact and "version" in artifact:
+            package = Package(artifact["name"], artifact["version"])
+
+            if "purl" in artifact:
+                package.add_purl(artifact["purl"])
+            if "cpes" in artifact:
+                for cpe in artifact["cpes"]:
+                    package.add_cpe(cpe)
+
+            package.generate_generic_cpe()
+            package.generate_generic_purl()
+
+            self.packagesCtrl.add(package)
+            return package.id
+
+    def parse_match_details(self, match_details: list) -> list:
+        """Parse the `matchDetails` part of grype JSON output."""
+        packages = []
+
+        for matchd in match_details:
+            searchedby = matchd.get("searchedBy", {})
+            if "Package" in searchedby:
+                found_pkg = searchedby.get("Package", {})
+                if "name" in found_pkg and "version" in found_pkg:
+                    package = Package(found_pkg["name"], found_pkg["version"])
+
+                    if "purl" in searchedby:
+                        package.add_purl(searchedby["purl"])
+                    if "cpes" in searchedby:
+                        for cpe in searchedby["cpes"]:
+                            package.add_cpe(cpe)
+
+                    found_match = matchd.get("found", {})
+                    if "purl" in found_match:
+                        package.add_purl(found_match["purl"])
+                    if "cpes" in found_match:
+                        for cpe in found_match["cpes"]:
+                            package.add_cpe(cpe)
+
+                    package.generate_generic_cpe()
+                    package.generate_generic_purl()
+
+                    self.packagesCtrl.add(package)
+                    packages.append(package.id)
+        return packages
+
+    def parse_vulnerability_section(self, vulnerability: dict):
+        """Parse the `vulnerability` part of grype JSON output."""
+        vuln_data = Vulnerability(
+            vulnerability.get("id", "").upper(),
+            "grype",
+            vulnerability.get("dataSource", "unknown"),
+            vulnerability.get("namespace", "unknown").lower()
+        )
+
+        for url in vulnerability.get("urls", []):
+            vuln_data.add_url(url)
+
+        vuln_data.add_text(vulnerability.get("description"), "description")
+
+        for cvss_score in vulnerability.get("cvss", []):
+            cvss_item = CVSS(
+                cvss_score.get("version"),
+                cvss_score.get("vector", ""),
+                cvss_score.get("source", "unknown"),
+                cvss_score.get("metrics", {}).get("baseScore", 0.0),
+                cvss_score.get("metrics", {}).get("exploitabilityScore", 0.0),
+                cvss_score.get("metrics", {}).get("impactScore", 0.0)
+            )
+            vuln_data.register_cvss(cvss_item)
+        vuln_data.severity_without_cvss(vulnerability.get("severity", "unknown").lower(), None, False)
+        return vuln_data
+
+    def load_from_dict(self, data: dict):
+        """Load the GrypeVulns object from a dictionary."""
+        for match in data.get("matches", []):
+
+            packages = []
+
+            if "artifact" in match:
+                pkg_id = self.parse_artifact_section(match["artifact"])
+                if pkg_id is not None:
+                    packages.append(pkg_id)
+
+            if "matchDetails" in match:
+                packages.extend(self.parse_match_details(match["matchDetails"]))
+
+            if "vulnerability" not in match:
+                continue
+
+            vuln_data = self.parse_vulnerability_section(match["vulnerability"])
+
+            if vuln_data.id == "" or len(packages) < 1:
+                continue
+
+            for package in packages:
+                vuln_data.add_package(package)
+
+            self.vulnerabilitiesCtrl.add(vuln_data)
+
+            assessment = self.assessmentsCtrl.gets_by_vuln_pkg(vuln_data.id, packages[0])
+            if len(assessment) < 1:
+                assessment = VulnAssessment(vuln_data.id, packages)
+                self.assessmentsCtrl.add(assessment)
---
+++ tests/integration_tests/test_grype_parsing.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+import pytest
+from src.views.grype_vulns import GrypeVulns
+from src.controllers.packages import PackagesController
+from src.controllers.vulnerabilities import VulnerabilitiesController
+from src.controllers.assessments import AssessmentsController
+import json
+
+
+@pytest.fixture
+def grype_parser():
+    controllers = {}
+    controllers["packages"] = PackagesController()
+    controllers["vulnerabilities"] = VulnerabilitiesController(controllers["packages"])
+    controllers["assessments"] = AssessmentsController(controllers["packages"], controllers["vulnerabilities"])
+    return GrypeVulns(controllers)
+
+
+def test_parse_empty_json(grype_parser):
+    grype_parser.load_from_dict(json.loads("""{
+        "matches": [
+            {
+                "vulnerability": { },
+                "relatedVulnerabilities": [],
+                "matchDetails": [],
+                "artifact": { }
+            }
+        ]
+    }"""))
+    assert len(grype_parser.packagesCtrl.packages) == 0
+    assert len(grype_parser.vulnerabilitiesCtrl.vulnerabilities) == 0
+    assert len(grype_parser.assessmentsCtrl.assessments) == 0
+
+
+def test_parse_invalid_model_json(grype_parser):
+    grype_parser.load_from_dict(json.loads("""{
+        "matches": [
+            {
+                "foo": [],
+                "bar": { }
+            }
+        ]
+    }"""))
+    assert len(grype_parser.packagesCtrl.packages) == 0
+    assert len(grype_parser.vulnerabilitiesCtrl.vulnerabilities) == 0
+    assert len(grype_parser.assessmentsCtrl.assessments) == 0
+
+
+def test_parse_artifact_json(grype_parser):
+    grype_parser.load_from_dict(json.loads("""{
+        "matches": [
+            {
+                "vulnerability": { },
+                "relatedVulnerabilities": [],
+                "matchDetails": [],
+                "artifact": {
+                    "id": "00a810ca5fed6ade",
+                    "name": "binutils",
+                    "version": "2.38",
+                    "type": "UnknownPackage",
+                    "locations": [],
+                    "language": "",
+                    "licenses": [],
+                    "cpes": [
+                        "cpe:2.3:a:*:binutils:2.38:*:*:*:*:*:*:*"
+                    ],
+                    "purl": "pkg:generic/binutils@2.38",
+                    "upstreams": []
+                }
+            },
+            {
+                "vulnerability": { },
+                "relatedVulnerabilities": [],
+                "matchDetails": [],
+                "artifact": {
+                    "id": "0be740738cf18168",
+                    "name": "cairo",
+                    "version": "1.16.0",
+                    "type": "UnknownPackage",
+                    "locations": [],
+                    "language": "",
+                    "licenses": [],
+                    "cpes": [
+                        "cpe:2.3:a:*:cairo:1.16.0:*:*:*:*:*:*:*"
+                    ],
+                    "purl": "pkg:generic/cairo@1.16.0",
+                    "upstreams": []
+                }
+            }
+        ]
+    }"""))
+    assert len(grype_parser.packagesCtrl.packages) == 2
+    assert len(grype_parser.vulnerabilitiesCtrl.vulnerabilities) == 0
+    assert len(grype_parser.assessmentsCtrl.assessments) == 0
+    assert "binutils@2.38" in grype_parser.packagesCtrl.packages
+    assert "cairo@1.16.0" in grype_parser.packagesCtrl.packages
+
+
+def test_parse_match_details_json(grype_parser):
+    grype_parser.load_from_dict(json.loads("""{
+        "matches": [
+            {
+                "vulnerability": { },
+                "relatedVulnerabilities": [],
+                "matchDetails": [
+                    {
+                        "type": "cpe-match",
+                        "matcher": "stock-matcher",
+                        "searchedBy": {
+                            "namespace": "nvd:cpe",
+                            "cpes": [
+                                "cpe:2.3:a:*:cairo:1.16.0:*:*:*:*:*:*:*"
+                            ],
+                            "Package": {
+                                "name": "cairo",
+                                "version": "1.16.0"
+                            }
+                        },
+                        "found": {
+                            "vulnerabilityID": "CVE-2020-35492",
+                            "versionConstraint": "< 1.17.4 (unknown)",
+                            "cpes": [
+                                "cpe:2.3:a:cairographics:cairo:*:*:*:*:*:*:*:*"
+                            ]
+                        }
+                    }
+                ],
+                "artifact": { }
+            },
+            {
+                "vulnerability": { },
+                "relatedVulnerabilities": [],
+                "matchDetails": [
+                    {
+                        "type": "purl-match",
+                        "matcher": "stock-matcher",
+                        "searchedBy": {
+                            "namespace": "unknown",
+                            "purl": "pkg:generic/MarkupSafe@2.1.1",
+                            "Package": {
+                                "name": "MarkupSafe",
+                                "version": "2.1.1"
+                            }
+                        },
+                        "found": {
+                            "vulnerabilityID": "CVE-2020-99999",
+                            "versionConstraint": "< 2.2.0 (unknown)",
+                            "purl": "pkg:python/MarkupSafe@2.1.1"
+                        }
+                    }
+                ],
+                "artifact": { }
+            }
+        ]
+    }"""))
+    assert len(grype_parser.packagesCtrl.packages) == 2
+    assert len(grype_parser.vulnerabilitiesCtrl.vulnerabilities) == 0
+    assert len(grype_parser.assessmentsCtrl.assessments) == 0
+    assert "cairo@1.16.0" in grype_parser.packagesCtrl.packages
+    cairo = grype_parser.packagesCtrl.packages.get("cairo@1.16.0")
+    assert "cpe:2.3:a:cairographics:cairo:*:*:*:*:*:*:*:*" in cairo
+
+
+def test_parse_vulnerability_json(grype_parser):
+    grype_parser.load_from_dict(json.loads("""{
+        "matches": [
+            {
+                "vulnerability": {
+                    "id": "CVE-2020-35492",
+                    "dataSource": "https://nvd.nist.gov/vuln/detail/CVE-2020-35492",
+                    "namespace": "nvd:cpe",
+                    "severity": "High",
+                    "urls": [
+                        "https://bugzilla.redhat.com/show_bug.cgi?id=1898396",
+                        "https://security.gentoo.org/glsa/202305-21"
+                    ],
+                    "description": "A flaw was found in cairo's image-compositor.c \
+in all versions prior to 1.17.4. This flaw allows an attacker who can provide a crafted \
+input file to cairo's image-compositor (for example, by convincing a user to open a file \
+in an application using cairo, or if an application uses cairo on untrusted input) to \
+cause a stack buffer overflow -> out-of-bounds WRITE. The highest impact from this vulnerability \
+is to confidentiality, integrity, as well as system availability.",
+                    "cvss": [
+                        {
+                            "source": "nvd@nist.gov",
+                            "type": "Primary",
+                            "version": "2.0",
+                            "vector": "AV:N/AC:M/Au:N/C:P/I:P/A:P",
+                            "metrics": {
+                                "baseScore": 6.8,
+                                "exploitabilityScore": 8.6,
+                                "impactScore": 6.4
+                            },
+                            "vendorMetadata": {}
+                        },
+                        {
+                            "source": "nvd@nist.gov",
+                            "type": "Primary",
+                            "version": "3.1",
+                            "vector": "CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H",
+                            "metrics": {
+                                "baseScore": 7.8,
+                                "exploitabilityScore": 1.8,
+                                "impactScore": 5.9
+                            },
+                            "vendorMetadata": {}
+                        }
+                    ],
+                    "fix": {
+                    "versions": [],
+                    "state": "unknown"
+                    },
+                    "advisories": []
+                },
+                "relatedVulnerabilities": [],
+                "matchDetails": [],
+                "artifact": {
+                    "id": "0be740738cf18168",
+                    "name": "cairo",
+                    "version": "1.16.0",
+                    "type": "UnknownPackage",
+                    "locations": [],
+                    "language": "",
+                    "licenses": [],
+                    "cpes": [
+                        "cpe:2.3:a:*:cairo:1.16.0:*:*:*:*:*:*:*"
+                    ],
+                    "purl": "pkg:generic/cairo@1.16.0",
+                    "upstreams": []
+                }
+            }
+        ]
+    }"""))
+    assert len(grype_parser.packagesCtrl.packages) == 1
+    assert len(grype_parser.vulnerabilitiesCtrl.vulnerabilities) == 1
+    assert len(grype_parser.assessmentsCtrl.assessments) == 1
+    assert "cairo@1.16.0" in grype_parser.packagesCtrl.packages
+    assert "CVE-2020-35492" in grype_parser.vulnerabilitiesCtrl.vulnerabilities
+    cve = grype_parser.vulnerabilitiesCtrl.get("CVE-2020-35492")
+    assert cve.severity["severity"] == "high"
+    assert len(cve.urls) == 2
+    assert len(cve.severity["cvss"]) == 2
+    assessment = grype_parser.assessmentsCtrl.gets_by_vuln("CVE-2020-35492")[0]
+    assert assessment.is_compatible_status("under_investigation")
