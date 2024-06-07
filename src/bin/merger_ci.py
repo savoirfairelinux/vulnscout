c4287aed42718ac36b5b65721bfb39743caa568f
Louis Maillard <lmaillard@savoirfairelinux.com>
2024-06-07 16:34:07 -0400

    merger_ci: add script to parse and merge vulns
    This script is intended to run standalone after a pre-merge operation.
    It parse packages, vulnerabilities and assessments from different
    sources, and finaly merge them into a single JSON file.
    Theses files can then be used by a web app or any other script.

---
+++ src/bin/merger_ci.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This python job is to aggregate packages, vulnerabilities and assessments from
+# sources files, aggregate them, enrich them with VEX info and output them to files.
+# Outputs files will be used by web API later. (see scan.sh)
+#
+# Copyright (C) 2024 Savoir-faire Linux, Inc.
+
+from ..views.grype_vulns import GrypeVulns
+from ..views.yocto_vulns import YoctoVulns
+from ..controllers.packages import PackagesController
+from ..controllers.vulnerabilities import VulnerabilitiesController
+from ..controllers.assessments import AssessmentsController
+import glob
+import json
+import os
+
+GRYPE_CDX_PATH = "/scan/tmp/vulns-cdx.grype.json"
+GRYPE_SPDX_PATH = "/scan/tmp/vulns-spdx.grype.json"
+YOCTO_FOLDER = "/scan/tmp/yocto_cve_check"
+OUTPUT_PATH = "/scan/tmp/vulns-merged.json"
+OUTPUT_PKG_PATH = "/scan/tmp/packages-merged.json"
+OUTPUT_VULN_PATH = "/scan/tmp/vulnerabilities-merged.json"
+OUTPUT_ASSESSEMENT_PATH = "/scan/tmp/assessments-merged.json"
+
+
+def post_treatment(controllers, files):
+    """Merge the vulnerabilities by vuln id."""
+    pass
+
+
+def read_inputs(controllers):
+    """Read from well-known files to grab vulnerabilities."""
+    scanGrype = GrypeVulns(controllers)
+    scanYocto = YoctoVulns(controllers)
+
+    with open(os.getenv("GRYPE_CDX_PATH", GRYPE_CDX_PATH), "r") as f:
+        scanGrype.load_from_dict(json.loads(f.read()))
+    with open(os.getenv("GRYPE_SPDX_PATH", GRYPE_SPDX_PATH), "r") as f:
+        scanGrype.load_from_dict(json.loads(f.read()))
+
+    for file in glob.glob(f"{os.getenv('YOCTO_FOLDER', YOCTO_FOLDER)}/*.json"):
+        with open(file, "r") as f:
+            scanYocto.load_from_dict(json.loads(f.read()))
+
+
+def output_results(controllers, files):
+    """Output the results to files."""
+    output = {
+        "packages": controllers["packages"].to_dict(),
+        "vulnerabilities": controllers["vulnerabilities"].to_dict(),
+        "assessments": controllers["assessments"].to_dict()
+    }
+    with open(os.getenv("OUTPUT_PATH", OUTPUT_PATH), "w") as f:
+        f.write(json.dumps(output))
+    with open(os.getenv("OUTPUT_PKG_PATH", OUTPUT_PKG_PATH), "w") as f:
+        f.write(json.dumps(output["packages"]))
+    with open(os.getenv("OUTPUT_VULN_PATH", OUTPUT_VULN_PATH), "w") as f:
+        f.write(json.dumps(output["vulnerabilities"]))
+    with open(os.getenv("OUTPUT_ASSESSEMENT_PATH", OUTPUT_ASSESSEMENT_PATH), "w") as f:
+        f.write(json.dumps(output["assessments"]))
+
+
+def main():
+    pkgCtrl = PackagesController()
+    vulnCtrl = VulnerabilitiesController(pkgCtrl)
+    assessCtrl = AssessmentsController(pkgCtrl, vulnCtrl)
+    controllers = {
+        "packages": pkgCtrl,
+        "vulnerabilities": vulnCtrl,
+        "assessments": assessCtrl
+    }
+
+    files = read_inputs(controllers)
+    post_treatment(controllers, files)
+    output_results(controllers, files)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/end_to_end_tests/test_merger_ci.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import pytest
+import json
+import os
+from src.bin.merger_ci import main
+from . import write_demo_files
+
+
+@pytest.fixture()
+def init_files(tmp_path):
+    files = {
+        "GRYPE_CDX_PATH": tmp_path / "cdx.grype.json",
+        "GRYPE_SPDX_PATH": tmp_path / "spdx.grype.json",
+        "YOCTO_FOLDER": tmp_path / "yocto_cve",
+        "YOCTO_CVE_CHECKER": tmp_path / "yocto_cve" / "demo.json",
+        "OUTPUT_PATH": tmp_path / "all-merged.json",
+        "OUTPUT_PKG_PATH": tmp_path / "packages-merged.json",
+        "OUTPUT_VULN_PATH": tmp_path / "vulnerabilities-merged.json",
+        "OUTPUT_ASSESSEMENT_PATH": tmp_path / "assessments-merged.json",
+    }
+    files["YOCTO_FOLDER"].mkdir()
+    write_demo_files(files)
+    return files
+
+
+def test_running_script(init_files):
+    for key, value in init_files.items():
+        os.environ[key] = str(value)
+
+    main()
+
+    out_all = json.loads(init_files["OUTPUT_PATH"].read_text())
+    out_pkg = json.loads(init_files["OUTPUT_PKG_PATH"].read_text())
+    out_vuln = json.loads(init_files["OUTPUT_VULN_PATH"].read_text())
+    out_assessment = json.loads(init_files["OUTPUT_ASSESSEMENT_PATH"].read_text())
+
+    assert "cairo@1.16.0" in out_pkg
+    assert "cairo@1.16.0" in out_all["packages"]
+    assert "busybox@1.35.0" in out_pkg
+    assert "busybox@1.35.0" in out_all["packages"]
+    assert "c-ares@1.18.1" in out_pkg
+    assert "c-ares@1.18.1" in out_all["packages"]
+
+    assert "CVE-2020-35492" in out_vuln
+    assert "CVE-2020-35492" in out_all["vulnerabilities"]
+    assert "CVE-2022-30065" in out_vuln
+    assert "CVE-2022-30065" in out_all["vulnerabilities"]
+    assert "CVE-2007-3152" in out_vuln
+    assert "CVE-2007-3152" in out_all["vulnerabilities"]
+    assert "CVE-2023-31124" in out_vuln
+    assert "CVE-2023-31124" in out_all["vulnerabilities"]
+
+    assert len(out_assessment) == 5
+    assert len(out_all["assessments"]) == len(out_assessment)
