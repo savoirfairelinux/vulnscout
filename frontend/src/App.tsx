6bc622fed1d4bac2901d02d2b4cf81e6910f5628
Louis Maillard <lmaillard@savoirfairelinux.com>
2024-06-10 13:12:37 -0400

    frontend: add basic vite, react, tailwindcss setup
    This commit adds a basic setup for a frontend using Vite, React and
    TailwindCSS. Because this basic frontend need at least one page, also
    include the loading page but without any loading logic.
    
    .gitkeep is needed because it will copy to static/ folder in the
    flask backend, and vite erase older content so we need it in front to
    be copied in backend on build step.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Loading from "./pages/Loading";
+
+
+/**
+ * The main application component
+ * @constructor
+ * @returns App A react component
+ */
+function App() {
+  return (
+    <Loading />
+  );
+}
+
+export default App
