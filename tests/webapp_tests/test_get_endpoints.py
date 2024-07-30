57d5533592a0d84cbc9fbfdeb3aa9078e8e466b6
Louis Maillard <louis.maillard@savoirfairelinux.com>
2024-07-30 14:54:07 -0400

    backend: add export of AsciiDoc directly to PDF
    Use asciidoctor and pdf plugin to be able to convert .adoc to .pdf
    using command line. Streamline the process using python subprocess to
    generate pdf file just by setting ?ext=pdf in API call.

--- tests/webapp_tests/test_get_endpoints.py
+++ tests/webapp_tests/test_get_endpoints.py
@@ -173,6 +173,11 @@ def test_render_document_adoc(client):
     assert "1 High" in content
 
 
+def test_render_document_pdf(client):
+    response = client.get("/api/documents/summary.adoc?ext=pdf")
+    assert response.status_code == 200
+
+
 def test_render_cdx_v1_6(client):
     response = client.get("/api/documents/CycloneDX 1.6?ext=json")
     assert response.status_code == 200
