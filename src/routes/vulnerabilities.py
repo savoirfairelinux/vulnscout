e58b2671bc10907db604951d32ee9106affab060
Louis Maillard <louis.maillard@savoirfairelinux.com>
2024-08-14 09:57:48 -0400

    api: add PATCH method to edit vulnerabilities
    Add a PATCH method to allow user to edit and save time estimations for
    a vulnerability. REST API should return the full object after
    modification, so I also added a GET method as the code was already here.
    It may be used  by thrid-party scripts to fetch informations about
    a vulnerability alone with it's ID.

--- src/routes/vulnerabilities.py
+++ src/routes/vulnerabilities.py
@@ -2,14 +2,18 @@ from flask import request
 import json
 from ..controllers.packages import PackagesController
 from ..controllers.vulnerabilities import VulnerabilitiesController
+from ..views.time_estimates import TimeEstimates
 
 VULNS_FILE = "/scan/tmp/vulnerabilities-merged.json"
+TIME_ESTIMATES_PATH = "/scan/outputs/time_estimates.json"
 
 
 def init_app(app):
 
     if "VULNS_FILE" not in app.config:
         app.config["VULNS_FILE"] = VULNS_FILE
+    if "TIME_ESTIMATES_PATH" not in app.config:
+        app.config["TIME_ESTIMATES_PATH"] = TIME_ESTIMATES_PATH
 
     @app.route('/api/vulnerabilities')
     def index_vulns():
@@ -19,3 +23,36 @@ def init_app(app):
             if request.args.get('format', 'list') == "dict":
                 return vulnsCtrl.to_dict()
             return list(vulnsCtrl.to_dict().values())
+
+    @app.route('/api/vulnerabilities/<id>', methods=['GET', 'PATCH'])
+    def update_vuln(id):
+        with open(app.config["VULNS_FILE"], "r") as f:
+            vulnsCtrl = VulnerabilitiesController.from_dict(PackagesController(), json.loads(f.read()))
+
+        vuln = vulnsCtrl.get(id)
+        if vuln:
+            if request.method == 'PATCH':
+                payload_data = request.get_json()
+                if "effort" in payload_data:
+                    if not ("optimistic" in payload_data["effort"]
+                       and "likely" in payload_data["effort"]
+                       and "pessimistic" in payload_data["effort"]):
+                        return "Invalid effort values", 400
+
+                    if not vuln.set_effort(
+                        payload_data["effort"]["optimistic"],
+                        payload_data["effort"]["likely"],
+                        payload_data["effort"]["pessimistic"]
+                    ):
+                        return "Invalid effort values", 400
+                with open(app.config["VULNS_FILE"], "w") as f:
+                    f.write(json.dumps(vulnsCtrl.to_dict()))
+                with open(app.config["TIME_ESTIMATES_PATH"], "w") as f:
+                    f.write(json.dumps(TimeEstimates({
+                        "packages": None,
+                        "vulnerabilities": vulnsCtrl,
+                        "assessments": None
+                    }).to_dict(), indent=2))
+
+            return vuln.to_dict()
+        return "Not found", 404
