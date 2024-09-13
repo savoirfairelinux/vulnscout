3dd16ee56de959c9ad499b229c694bc5926a5e65
Louis Maillard <louis.maillard@savoirfairelinux.com>
2024-09-13 13:10:08 -0400

    patch-finder: add scrapper to find fixed versions
    Class FixsScrapper is able to search for versions (either fixed or
    vulnerable versions) in text and NVD entries.

---
+++ src/helpers/fixs_scrapper.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+from ..models.vulnerability import Vulnerability
+import re
+from typing import Optional
+
+
+class FixSolution:
+    """
+    Represent a list of vulnerable and fixed versions associated to a package
+    """
+
+    def __init__(self, package: str, scrapper: str):
+        self.package: str = package
+        self.fixed: list[str] = []
+        self.vulnerables: list[str] = []
+        self.scrapper: str = scrapper
+
+
+class FixsScrapper:
+    """
+    A serie of methods for scrapping data in Vulnerabilities content, NVD API, ...
+    Main goal is to find information to help fixing vulnerability, like version where it's fixed
+    """
+
+    semver_regex = re.compile(r"""
+        (before|through|until|after|from|prior\sto)?  # context keywords (\s = space)
+        \s*                                # optional whitespace
+        v?                                 # optional v
+        (\d+)                              # major
+        \.(\d+|x)                          # minor (number or x)
+        (\.(\d+|x))?                       # patch (optional, number or x)
+        """, re.VERBOSE | re.IGNORECASE)
+    """
+    Regex to parse version and their context in a string
+    first group contain context keywords if any, second major, third minor, and patch is eventualy in five
+    """
+
+    def __init__(self):
+        self.solutions: list[FixSolution] = []
+
+    def search_in_vulnerability(self, vulnerability: Vulnerability):
+        """
+        Search in a vulnerability description / texts to find a fix or solution
+        """
+        for title in vulnerability.texts:
+            matchs = re.findall(self.semver_regex, vulnerability.texts[title])
+            for match in matchs:
+                context = match[0]
+                new_version = f"{match[1]}.{match[2]}"
+                if len(match) >= 5 and match[4] != '':
+                    new_version += f".{match[4]}"
+                for pkg in vulnerability.packages:
+                    pkg_parts = pkg.split("@")
+                    artifact = FixSolution(pkg_parts[0], "local-description-regex")
+                    if context == "before" or context == "prior to":
+                        artifact.fixed.append(f">=? {new_version}")
+                        artifact.vulnerables.append(f"<? {new_version}")
+                    elif context == "through":
+                        artifact.fixed.append(f">? {new_version}")
+                        artifact.vulnerables.append(f"<=? {new_version}")
+                    elif context == "after" or context == "from":
+                        artifact.fixed.append(f"<? {new_version}")
+                        artifact.vulnerables.append(f">=? {new_version}")
+                    else:
+                        artifact.fixed.append(f"{context}? {new_version}")
+                        artifact.vulnerables.append(f"{context}? {new_version}")
+                    self.solutions.append(artifact)
+
+    def _extract_from_criteria(self, criteria: str) -> Optional[tuple[str, str]]:
+        """
+        Return package name and version from a CPE string, or None
+        Internal use only
+        """
+        if criteria.startswith("cpe:2.3"):
+            parts = criteria.split(":")
+            if len(parts) >= 7:
+                return parts[4], parts[5]
+        return None
+
+    def _search_in_nvd_node(self, node: dict, negate: bool = False):
+        """
+        Generate a list of FixSolution from a NVD node
+        Internal use only
+        """
+        is_negated = negate
+        if "nodes" in node:
+            for child in node["nodes"]:
+                self._search_in_nvd_node(child, negate)
+            return
+        if "negate" in node and node["negate"]:
+            is_negated = not is_negated
+        if "cpeMatch" in node:
+            for match in node["cpeMatch"]:
+                res = self._extract_from_criteria(match["criteria"])
+                if res is None:
+                    continue
+                pkg_name, pkg_version = res
+                artifact = FixSolution(pkg_name, "nvd-cpe-match")
+                store_as_fixed = is_negated  # False by default, unless we are negated
+                if "vulnerable" in match and not match["vulnerable"]:
+                    store_as_fixed = not store_as_fixed
+
+                if "versionEndIncluding" in match:
+                    if not store_as_fixed:
+                        artifact.fixed.append(f">? {match['versionEndIncluding']}")
+                        artifact.vulnerables.append(f"<= {match['versionEndIncluding']}")
+                    else:
+                        artifact.fixed.append(f"<= {match['versionEndIncluding']}")
+                        artifact.vulnerables.append(f">? {match['versionEndIncluding']}")
+
+                if "versionEndExcluding" in match:
+                    if not store_as_fixed:
+                        artifact.fixed.append(f">=? {match['versionEndExcluding']}")
+                        artifact.vulnerables.append(f"< {match['versionEndExcluding']}")
+                    else:
+                        artifact.fixed.append(f"< {match['versionEndExcluding']}")
+                        artifact.vulnerables.append(f">=? {match['versionEndExcluding']}")
+
+                if "versionStartIncluding" in match:
+                    if not store_as_fixed:
+                        artifact.fixed.append(f"<? {match['versionStartIncluding']}")
+                        artifact.vulnerables.append(f">= {match['versionStartIncluding']}")
+                    else:
+                        artifact.fixed.append(f">= {match['versionStartIncluding']}")
+                        artifact.vulnerables.append(f"<? {match['versionStartIncluding']}")
+
+                if "versionStartExcluding" in match:
+                    if not store_as_fixed:
+                        artifact.fixed.append(f"<=? {match['versionStartExcluding']}")
+                        artifact.vulnerables.append(f"> {match['versionStartExcluding']}")
+                    else:
+                        artifact.fixed.append(f"> {match['versionStartExcluding']}")
+                        artifact.vulnerables.append(f"<=? {match['versionStartExcluding']}")
+
+                if pkg_version != "*" and pkg_version != "-" and pkg_version != "":
+                    if store_as_fixed:
+                        artifact.fixed.append(f"= {pkg_version}")
+                    else:
+                        artifact.vulnerables.append(f"= {pkg_version}")
+                self.solutions.append(artifact)
+
+    def search_in_nvd(self, nvd_results: dict):
+        """
+        Generate a list of FixSolution from a result from NVD API
+        """
+        if "vulnerabilities" in nvd_results:
+            for vuln in nvd_results["vulnerabilities"]:
+                self.search_in_nvd(vuln)
+            return
+        if "cve" in nvd_results and "configurations" in nvd_results["cve"]:
+            childs = nvd_results["cve"]["configurations"]
+            for child in childs:
+                self._search_in_nvd_node(child)
+
+    def list_fixing_versions(self) -> list[str]:
+        """
+        Return a list of all fixing contraint from solutions
+        """
+        constrains = list(set([f"{solution.package} {x}" for solution in self.solutions for x in solution.fixed]))
+        sorted_constrains = sorted(constrains, key=lambda x: x.split()[-1])
+        return sorted_constrains
+
+    def list_vulnerables_versions(self) -> list[str]:
+        """
+        Return a list of all affected versions from solutions
+        """
+        constrains = list(set([f"{solution.package} {x}" for solution in self.solutions for x in solution.vulnerables]))
+        sorted_constrains = sorted(constrains, key=lambda x: x.split()[-1])
+        return sorted_constrains
+
+    def list_per_packages(self) -> dict[str, dict[str, list[str]]]:
+        """
+        Return a dict with fix and affected versions from solutions, sorted by package and scrapper
+        """
+        solutions: dict = {}
+        for solution in self.solutions:
+            key = f"{solution.package} ({solution.scrapper})"
+            if key not in solutions:
+                solutions[key] = {"fix": [], "affected": []}
+            for fixed in solution.fixed:
+                if fixed not in solutions[key]["fix"]:
+                    solutions[key]["fix"].append(fixed)
+            for vulnerable in solution.vulnerables:
+                if vulnerable not in solutions[key]["affected"]:
+                    solutions[key]["affected"].append(vulnerable)
+        return solutions
---
+++ tests/unit_tests/test_fixs_scrapper.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+import pytest
+from src.helpers.fixs_scrapper import FixsScrapper
+from src.models.vulnerability import Vulnerability
+import json
+
+
+@pytest.fixture
+def ex_nvd_simple():
+    # CVE-2003-0063
+    return json.loads("""{ "cve": {
+        "configurations": [
+            {"nodes": [
+                {
+                    "operator": "OR",
+                    "negate": false,
+                    "cpeMatch": [
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:xfree86_project:x11r6:4.0:*:*:*:*:*:*:*"
+                        },
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:xfree86_project:x11r6:4.0.1:*:*:*:*:*:*:*"
+                        },
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:xfree86_project:x11r6:4.2.1:*:*:*:*:*:*:*"
+                        }
+                    ]
+                }
+            ]}
+        ]
+    } }""")
+
+
+@pytest.fixture
+def ex_nvd_start_end():
+    # CVE-2022-29847
+    return json.loads("""{ "cve": {
+        "configurations": [
+            {"nodes": [
+                {
+                    "operator": "OR",
+                    "negate": false,
+                    "cpeMatch": [
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:progress:whatsup_gold:*:*:*:*:*:*:*:*",
+                            "versionStartIncluding": "21.0.0",
+                            "versionEndIncluding": "21.1.1"
+                        },
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:progress:whatsup_gold:22.0.0:*:*:*:*:*:*:*"
+                        },
+                        {
+                            "vulnerable": false,
+                            "criteria": "cpe:2.3:a:progress:whatsup_gold:22.2.0:*:*:*:*:*:*:*"
+                        },
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:progress:whatsup_gold:*:*:*:*:*:*:*:*",
+                            "versionStartExcluding": "123.1.0",
+                            "versionEndExcluding": "123.3.0"
+                        }
+                    ]
+                }
+            ]}
+        ]
+    } }""")
+
+
+@pytest.fixture
+def ex_nvd_inversed():
+    return json.loads("""{ "cve": {
+        "configurations": [
+            {"nodes": [
+                {
+                    "operator": "OR",
+                    "negate": false,
+                    "cpeMatch": [
+                        {
+                            "vulnerable": false,
+                            "criteria": "cpe:2.3:a:*:abc:*:*:*:*:*:*:*:*",
+                            "versionStartExcluding": "1.1.0",
+                            "versionEndExcluding": "1.3.0"
+                        }
+                    ]
+                },
+                {
+                    "operator": "OR",
+                    "negate": true,
+                    "cpeMatch": [
+                        {
+                            "vulnerable": true,
+                            "criteria": "cpe:2.3:a:*:abc:*:*:*:*:*:*:*:*",
+                            "versionStartIncluding": "2.0.0",
+                            "versionEndIncluding": "2.1.1"
+                        }
+                    ]
+                }
+            ]}
+        ]
+    } }""")
+
+
+@pytest.fixture
+def ex_nvd_not_cpe():
+    return json.loads("""{ "cve": {
+        "configurations": [
+            {"nodes": [
+                {
+                    "operator": "OR",
+                    "negate": false,
+                    "cpeMatch": [
+                        {
+                            "vulnerable": true,
+                            "criteria": "some invalid value"
+                        }
+                    ]
+                }
+            ]}
+        ]
+    } }""")
+
+
+def test_simple_nvd_scan(ex_nvd_simple):
+    fxs = FixsScrapper()
+    fxs.search_in_nvd(ex_nvd_simple)
+    assert len(fxs.list_fixing_versions()) == 0
+    assert len(fxs.list_vulnerables_versions()) == 3
+    assert [
+        "x11r6 = 4.0",
+        "x11r6 = 4.0.1",
+        "x11r6 = 4.2.1"
+    ] == fxs.list_vulnerables_versions()
+
+
+def test_start_end_nvd_scan(ex_nvd_start_end):
+    fxs = FixsScrapper()
+    fxs.search_in_nvd(ex_nvd_start_end)
+    assert len(fxs.list_fixing_versions()) == 5
+    assert [
+        "whatsup_gold <=? 123.1.0",
+        "whatsup_gold >=? 123.3.0",
+        "whatsup_gold <? 21.0.0",
+        "whatsup_gold >? 21.1.1",
+        "whatsup_gold = 22.2.0"
+    ] == fxs.list_fixing_versions()
+    assert len(fxs.list_vulnerables_versions()) == 5
+    assert [
+        "whatsup_gold > 123.1.0",
+        "whatsup_gold < 123.3.0",
+        "whatsup_gold >= 21.0.0",
+        "whatsup_gold <= 21.1.1",
+        "whatsup_gold = 22.0.0"
+    ] == fxs.list_vulnerables_versions()
+
+
+def test_inversed_nvd_scan(ex_nvd_inversed):
+    fxs = FixsScrapper()
+    fxs.search_in_nvd(ex_nvd_inversed)
+    assert len(fxs.list_fixing_versions()) == 4
+    assert [
+        "abc > 1.1.0",
+        "abc < 1.3.0",
+        "abc >= 2.0.0",
+        "abc <= 2.1.1"
+    ] == fxs.list_fixing_versions()
+    assert len(fxs.list_vulnerables_versions()) == 4
+    assert [
+        "abc <=? 1.1.0",
+        "abc >=? 1.3.0",
+        "abc <? 2.0.0",
+        "abc >? 2.1.1"
+    ] == fxs.list_vulnerables_versions()
+
+
+def test_not_cpe_nvd_scan(ex_nvd_not_cpe):
+    fxs = FixsScrapper()
+    fxs.search_in_nvd(ex_nvd_not_cpe)
+    assert len(fxs.list_fixing_versions()) == 0
+    assert len(fxs.list_vulnerables_versions()) == 0
+
+
+def test_all_cve_scan(ex_nvd_simple, ex_nvd_start_end, ex_nvd_inversed, ex_nvd_not_cpe):
+    fxs = FixsScrapper()
+    fxs.search_in_nvd({
+        "vulnerabilities": [
+            ex_nvd_simple,
+            ex_nvd_start_end,
+            ex_nvd_inversed,
+            ex_nvd_not_cpe
+        ]
+    })
+    soluces = fxs.list_per_packages()
+    assert len(soluces) == 3
+    assert "whatsup_gold (nvd-cpe-match)" in soluces
+    assert len(soluces["whatsup_gold (nvd-cpe-match)"]["fix"]) == 5
+    assert len(soluces["x11r6 (nvd-cpe-match)"]["affected"]) == 3
+
+
+def test_find_in_text():
+    vuln = Vulnerability("CVE-0000-00000", "test", "demo", "demo")
+    vuln.add_package("abc@1.0.0")
+    vuln.add_text(
+        "A security issue in main.c affecting version prior to 1.2.3 can lead to remote code execution",
+        "test1"
+    )
+    vuln.add_text("All versions after 2.0 through 2.4.2 are affected", "test2")
+    vuln.add_text("A temporary patch was released in version 3.2.7 and 3.3.2", "test3")
+    fxs = FixsScrapper()
+    fxs.search_in_vulnerability(vuln)
+
+    assert len(fxs.list_fixing_versions()) == 5
+    assert [
+        "abc >=? 1.2.3",
+        "abc <? 2.0",
+        "abc >? 2.4.2",
+        "abc ? 3.2.7",
+        "abc ? 3.3.2"
+    ] == fxs.list_fixing_versions()
+    assert len(fxs.list_vulnerables_versions()) == 5
+    assert [
+        "abc <? 1.2.3",
+        "abc >=? 2.0",
+        "abc <=? 2.4.2",
+        "abc ? 3.2.7",
+        "abc ? 3.3.2"
+    ] == fxs.list_vulnerables_versions()
