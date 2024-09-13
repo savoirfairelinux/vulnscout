844f5fa9ef16f2505d21defaa151aaa40f72780c
Louis Maillard <louis.maillard@savoirfairelinux.com>
2024-09-13 13:37:06 -0400

    api: add route to scan vulns and return patch info
    Add a new route in POST which allow scanning a list of CVE to find
    package affected and their fixed versions. It read data from sqlite DB.
    Added mini_nvd.db (size of 20 Ko, contain ~15 vulns of binutils) for
    unit testing.

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,6 +2,7 @@ from .packages import init_app as init_pkg_app
 from .vulnerabilities import init_app as init_vuln_app
 from .assessments import init_app as init_assess_app
 from .documents import init_app as init_doc_app
+from .patch_finder import init_app as init_patch_finder_app
 from .frontpage import init_app as init_front_app
 
 
@@ -10,6 +11,7 @@ def init_app(app):
     init_vuln_app(app)
     init_assess_app(app)
     init_doc_app(app)
+    init_patch_finder_app(app)
     # keep front endpoint at the end
     init_front_app(app)
     return app
