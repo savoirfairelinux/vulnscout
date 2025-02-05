ec2938351d2aa716ca0f6721879fbddeee56f6d1
Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>
2025-02-05 09:19:03 +0100

    nvd_db_builder: fix use of NVD_API_KEY
    Since commit [1], the NVD_API_KEY environment variable is set by
    default to an empty string in the Dockerfile. It allows configuring a
    specific API key. However, in case there is no such custom key,
    the nvd_api_key would be set to an empty string instead of None.
    Vulnscout would therefore query the NVD with an empty api key field,
    always returning a 404 error.
    
    Fix this by assigning nvd_db.nvd_api_key with os.getenv("NVD_API_KEY")
    only if it not None and not empty.
    
    [1]: https://github.com/savoirfairelinux/vulnscout/commit/699ad8de03f6db9f118f31082ae47685a48acc80

--- src/bin/nvd_db_builder.py
+++ src/bin/nvd_db_builder.py
@@ -10,9 +10,11 @@ import os
 def fetch_db_updates():
     nvd_db_path = os.getenv("NVD_DB_PATH", "/cache/vulnscout/nvd.db")
     nvd_db = NVD_DB(nvd_db_path)
-    nvd_db.nvd_api_key = os.getenv("NVD_API_KEY")
-    if not nvd_db.nvd_api_key:
+    nvd_api_key = os.getenv("NVD_API_KEY")
+    if not nvd_api_key:
         print("NVD API key not found, this may slow down db update. See NVD_API_KEY configuration")
+    else:
+        nvd_db.nvd_api_key = nvd_api_key
     nvd_db.set_writing_flag(True)
     for step, total in nvd_db.build_initial_db():
         print(f"NVD update: {step} / {total} [{round((step / total) * 100)}%]", flush=True)
