2209ff68bff8cc1338df13084e45c324a3288dbb
Rawaha El Houssayni <rawaha.elhoussayni@savoirfairelinux.com>
2026-03-26 16:11:42 -0400

    Add default project value for the new entrypoint

--- src/routes/config.py
+++ src/routes/config.py
@@ -26,6 +26,10 @@ def init_app(app):
                 variants = VariantController.get_by_project(project.id)
                 variant = next((v for v in variants if v.name == variant_name), None)
 
+        if not project:
+            all_projects = ProjectController.get_all()
+            project = all_projects[0] if all_projects else None
+
         return jsonify({
             "project": ProjectController.serialize(project) if project else None,
             "variant": VariantController.serialize(variant) if variant else None,
