b74fdfaad51af59ab8cc4aadecd91a314c2526f1
ValentinBoudevinSFL <valentin.boudevin@savoirfairelinux.com>
2026-04-22 14:47:31 -0400

    fix: Modified Copyright date to 2026
    Change-Id: I1643ab393ae37c9e823d83ea77c11bf51adf6a69

--- tests/webapp_tests/test_scan_triggers.py
+++ tests/webapp_tests/test_scan_triggers.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2024 Savoir-faire Linux, Inc.
+# Copyright (C) 2026 Savoir-faire Linux, Inc.
 # SPDX-License-Identifier: GPL-3.0-only
 
 """Tests for Grype / NVD / OSV scan trigger & status endpoints in scans.py."""
